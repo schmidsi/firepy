ed9483e6bf2d08069ef26b2104826e9f038c8ee0
serialx.net <serialx.net@bd028940-396c-11de-97fb-aff7d2a6cc54>
2009-06-16 11:00:15 +0000

    Modified the setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='firepy', version='0.1',
-      install_requires=['simplejson']
+setup(
+        name='firepy',
+        version='0.1.2',
+        description='FirePHP for Python',
+        long_description=('This is a python server library for FirePHP '
+                          'supporting python built-in logging facility '
+                          'and Django.'),
+        author='Sung-jin Hong',
+        author_email='serialx@serialx.net',
+        license='MIT',
+        url='http://code.google.com/p/firepy/',
+        download_url='http://code.google.com/p/firepy/downloads/list',
+        packages=find_packages(),
+        zip_safe=False,
+        install_requires=[
+            'simplejson'
+        ]
         )
